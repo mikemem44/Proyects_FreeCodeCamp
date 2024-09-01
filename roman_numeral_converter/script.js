b2dec6e17931412e72484acc69031dcb0be85074
mikemem44 <mikelmm1999@gmail.com>
2024-09-01 00:58:07 -0600

    feat: Finished roman converter excercise

--- roman_numeral_converter/script.js
+++ roman_numeral_converter/script.js
@@ -1,8 +1,65 @@
 const numberInput = document.getElementById("number");
 const convertBtn = document.getElementById("convert-btn");
 const outputDiv = document.getElementById("output");
+const romanConversionValues = [{
+   decimalValue: 1000,
+   romanValue: "M"
+},
+{
+   decimalValue: 900,
+   romanValue: "CM"
+},
+{
+   decimalValue: 500,
+   romanValue: "D"
+},
+{
+   decimalValue: 400,
+   romanValue: "CD"
+},
+{
+   decimalValue: 100,
+   romanValue: "C"
+},
+{
+   decimalValue: 90,
+   romanValue: "XC"
+},
+{
+   decimalValue: 50,
+   romanValue: "L"
+},
+{
+   decimalValue: 40,
+   romanValue: "XL"
+},
+{
+   decimalValue: 10,
+   romanValue: "X"
+},
+{
+   decimalValue: 9,
+   romanValue: "IX"
+},
+{
+   decimalValue: 5,
+   romanValue: "V"
+},
+{
+   decimalValue: 4,
+   romanValue: "IV"
+},
+{
+   decimalValue: 1,
+   romanValue: "I"
+}];
 
-const convertToRoman= () => {
+const outputValue = [];
+let quotient;
+let remainer;
+let stopLoop = false;
+
+const buttonClicked= () => {
      const number = parseInt(numberInput.value.trim());
 
      if (!numberInput.value){
@@ -11,18 +68,36 @@ const convertToRoman= () => {
         outputDiv.textContent = "Please enter a number greater than or equal to 1";
      }else if(number > 3999){
         outputDiv.textContent =  "Please enter a number less than or equal to 3999";
-     }
-        
+     }else{
+         conversion(number);      
+         outputDiv.textContent = outputValue.join("");
+         outputValue.splice(0,outputValue.length); 
+         stopLoop = false;
+   }
 }
 
-const conversion = (number) => {
 
+const conversion = (input) => {
+   romanConversionValues.forEach((obj) => {
+      if (input >= obj.decimalValue && stopLoop === false){
+         quotient = Math.floor(input / obj.decimalValue);
+         outputValue.push(obj.romanValue.repeat(quotient));
+         remainer = input % obj.decimalValue;
+         stopLoop = true;
+      }
+   })
+
+   if (remainer !== 0){
+      stopLoop = false;
+      conversion(remainer);
+      return;
+   }
 }
 
-convertBtn.addEventListener("click", convertToRoman);
+convertBtn.addEventListener("click", buttonClicked);
 
 numberInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
-      convertToRoman();
+      buttonClicked();
     }
   });
